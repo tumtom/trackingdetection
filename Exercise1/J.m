40b6724e7edf1480f7b99f26dc3826c7641e169e
tom <thomas.rehner@tum.de>
2016-10-27 16:55:38 +0200

    fixed bug in variable naming, added removal of added border, image is now shown after executing

--- Exercise1/J.m
+++ Exercise1/J.m
@@ -1,13 +1,13 @@
 function v = J ( I,H,x,y )
 v=0;
 [m, n] = size(H);
-middle_rows = ceil(m/2);
+middle_rows = ceil(m/2);    %these are redundantly calculated each time the func is called
 middle_cols = ceil(n/2);
 
-%in outer loop the rows starting upper row
+%in outer loop trough the rows starting with upper row
 for i=1:m
     offset_rows = i - middle_rows;
-    %loop trough the columns starting left
+    %loop trough the columns starting at left side
     for j=1:n
         offset_cols = j - middle_cols;
         v = v+H(i,j)*I(x - offset_rows, y - offset_cols);
--- Exercise1/exercise1.m
+++ Exercise1/exercise1.m
@@ -1,23 +1,27 @@
 %config
-H = [[1,1,1];[1,1,1];[1,1,1]];
+H = [
+    [0,0,0];
+    [0,1,0];
+    [0,0,0]
+    ];
 image_path = 'lena.gif';
 
 
 %beginning of script
 
 [m,n] = size(H);
-I_raw = imread(image_path);
+[I_raw,I_raw_cm] = imread(image_path);
 
 %preparing the image
 [w,h] = size(I_raw);
-I = zeros(M+w,N+h);
+I = zeros(m+w,n+h);
 
 %copy old image in the middle
 image_start_row = ceil(m/2);
 image_start_col = ceil(n/2);
-image_end_row = image_start_row+w;
-image_end_col = image_start_col+h;
-I(image_start_row:image_end_row-1,image_start_col:image_end_col-1) = I_raw(1:w,1:h);
+image_end_row = image_start_row+w-1;
+image_end_col = image_start_col+h-1;
+I(image_start_row:image_end_row,image_start_col:image_end_col) = I_raw(1:w,1:h);
 
 %create new image
 R = zeros(size(I));
@@ -30,3 +34,10 @@ for x=image_start_row:image_end_row
         R(x,y)=v;
     end
 end
+
+%remove the added border 
+RES = zeros(size(I_raw));
+RES(1:size(I_raw,1),1:size(I_raw,2)) = R(image_start_row:image_end_row,image_start_col:image_end_col);
+
+%show the result
+imshow(RES,I_raw_cm);
