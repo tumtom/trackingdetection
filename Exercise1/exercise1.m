d176f092d0303c4b08321433fac1e600fb43c8f3
tom <thomas.rehner@tum.de>
2016-11-02 22:10:48 +0100

    gradient orientation looks a little bit better now - saving now without colormap and brightness adaption. some comments added, some filenames changed.

--- Exercise1/exercise1.m
+++ Exercise1/exercise1.m
@@ -1,4 +1,5 @@
-%kernel
+%% task 1 - convolution and mean filter
+
 H = [
     [1,1,1];
     [1,1,1];
@@ -26,6 +27,9 @@ title('convolution');
 
 imwrite(MeanFilter,I_raw_cm,'Mean.png');
 
+%% Task 2 - Gaussian Filter
+
+
 sigma = 1.0;
 GaussianFilterKernel_Sigma1 = CreateGaussian(sigma);
 sigma = 3.0;
@@ -37,7 +41,6 @@ time_2D = toc(start_2D);
 imwrite(Gauss_Sigma1,I_raw_cm,'Gauss1.png')
 imwrite(Gauss_Sigma3,I_raw_cm,'Gauss3.png')
 
-
 sigma = 1.0;
 GaussianFilterKernel1D_Sigma1 = CreateGaussian1D(sigma).';
 sigma = 3.0;
@@ -48,14 +51,24 @@ Gauss_Sigma13 = convolve(Gauss_Sigma1,GaussianFilterKernel1D_Sigma3,'border_mirr
 time_1D = toc(start_1D);
 imwrite(Gauss_Sigma13,I_raw_cm,'Gauss1D_13.png')
 
+%% task 3 - gradient
+
 [magnitude,orientation] = gradient(I_raw);
+imwrite(orientation,'Gradient_orient_2.png');
 magnitude =  (256 / max(max(magnitude)) - min(min(magnitude))) * (magnitude - min(min(magnitude))); 
 orientation =  (256 / max(max(orientation)) - min(min(orientation))) * (orientation + pi/2); 
-imwrite(magnitude,I_raw_cm,'Gradient_mag.png')
-imwrite(orientation,I_raw_cm,'Gradient_orient.png')
+imwrite(magnitude,I_raw_cm,'Gradient_mag.png');
+imwrite(orientation,I_raw_cm,'Gradient_orient.png');
+
 
 D_x = [[-1,0,1];[-1,0,1];[-1,0,1]];
 
-G_x = convolve(GaussianFilterKernel_Sigma3, D_x, 'border_extend');
-I1 = convolve(I_raw,convolve(G_x,D_x), 'border_extend');
-imwrite(I1,I_raw_cm,'I1.png');
+G_x = convolve(GaussianFilterKernel_Sigma3, D_x, 'border_mirror');
+I1 = convolve(I_raw,G_x, 'border_mirror');
+I1 = convolve(I1,D_x, 'border_mirror');
+imwrite(I1,I_raw_cm,'Gradient_w_Gauss3.png');
+
+G_x = convolve(GaussianFilterKernel_Sigma3, D_x, 'border_mirror');
+G_x_D_x = convolve(G_x,D_x, 'border_mirror');
+I2 = convolve(I1,G_x_D_x, 'border_mirror');
+imwrite(I2,I_raw_cm,'Gradient_w_Gauss3_2.png');
