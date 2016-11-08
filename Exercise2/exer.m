78d0d2763e9c400e19d09526fad5965d49872241
tom <thomas.rehner@tum.de>
2016-11-08 14:08:07 +0100

    changes from meeting at tuesday

--- Exercise2/exer.m
+++ Exercise2/exer.m
@@ -2,16 +2,35 @@
 
 
 
-I_check = double(imread('checkerboard_tunnel.png'));
+I_check = double(imread('check.png'));
 
 I = I_check;
-sigma_d = 3;
+sigma_d = 1;
 sigma_i = 3;
-alpha = 0.05;
+alpha = 0.04;
 threshold = 1 * 10^7;
+N = 50;
 
-corners = harris(I, sigma_d, sigma_i, alpha, threshold);
+%corners = harris(I, sigma_d, sigma_i, alpha, threshold);
+corners = moment_matrix( I, 0, 0, alpha, sigma_d, sigma_i);
+imshow(corners);
 
-imagesc(I);
-hold on;
-plot(corners(:,1),corners(:,2));
+%50 largest
+%[corners_sorted,corners_pos] = sort(abs(corners(:)),'descend');
+%largest = corners_sorted(1:N);
+%largest_pos = corners_pos(1:N);
+%imagesc(I);
+%hold on;
+%for i=1:N
+%    [x,y] = ind2sub(size(corners),largest_pos(i));
+%    s=scatter(x,y,50);
+%end
+%hold off;
+
+%matlab harris
+%corners = detectHarrisFeatures(I);
+%imshow(I); hold on;
+%plot(corners.selectStrongest(50));
+
+
+%plot(corners(:,1),corners(:,2));
--- Exercise2/moment_matrix.m
+++ Exercise2/moment_matrix.m
@@ -1,4 +1,4 @@
-function [ M ] = moment_matrix ( I, x, y, sigma_d, sigma_i )
+function [ M ] = moment_matrix ( I, x, y, k, sigma_d, sigma_i )
 %calculates the moment-matrix for:
 % - I: image matrix (doubles!)
 % - x: image row
@@ -10,37 +10,41 @@ function [ M ] = moment_matrix ( I, x, y, sigma_d, sigma_i )
 persistent configuration;
 persistent D_x; 
 persistent D_y;
-persistent L_x;
-persistent L_y;
+persistent L_x2;
+persistent L_y2;
+persistent L_xy;
 persistent G_sigma_d;
 persistent G_Kernel_sigma_d;
 persistent G_Kernel_sigma_i;
 
 if isempty(D_x)
-    D_x = [[-1,0,1];[-1,0,1];[-1,0,1]];
+     D_x = [[-1,0,1];[-1,0,1];[-1,0,1]];
 end
 
 if isempty(D_y)
-    D_y = [[-1,0,1];[-1,0,1];[-1,0,1]].';
+     D_y = [[-1,0,1];[-1,0,1];[-1,0,1]].';
 end
 
 conf = [sigma_d,sigma_i];
 
-if  isempty(configuration) || ~ isequal(configuration,conf);
-    G_Kernel_sigma_d = CreateGaussian(sigma_d);
-    G_Kernel_sigma_i = CreateGaussian(sigma_i);
+if  isempty(configuration) || ~ isequal(configuration,conf)
+    %G_Kernel_sigma_d = CreateGaussian(sigma_d);
+    G_Kernel_sigma_d = fspecial('gaussian',max(1,fix(3*sigma_d)),sigma_d);
+    %G_Kernel_sigma_i = CreateGaussian(sigma_i);
 
-    G_sigma_d = conv2(I,G_Kernel_sigma_d,'same');
+    %G_sigma_d = conv2(I,G_Kernel_sigma_d,'same');
     %G_sigma_i = conv2(I,G_Kernel_sigma_i,'same');
    
-    L_x = conv2(G_sigma_d,D_x,'same');
-    L_y = conv2(G_sigma_d,D_y,'same');
+    I_x = conv2(I,D_x,'same');
+    I_y = conv2(I,D_y,'same');
     
+    L_x2 = conv2(I_x.^2,G_Kernel_sigma_d,'same');
+    L_y2 = conv2(I_y.^2,G_Kernel_sigma_d,'same');
+    L_xy = conv2(I_y.*I_y,G_Kernel_sigma_d,'same');
    configuration = [sigma_d,sigma_i];
 end
 
-Part = [  [ L_x(x,y)^2,  L_x(x,y)*L_y(x,y) ]; [ L_x(x,y)*L_y(x,y), L_y(x,y)^2  ]  ];
-M = (sigma_d)^2  * conv2(Part,G_Kernel_sigma_i) ;
+M = (L_x2.*L_y2 - (L_xy.^2)) - k * ((L_x2 + L_y2).^2);
 
 
 end
