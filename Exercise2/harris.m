a601f1ca66fa2fbabd1ecbec4b23b96da927ff7e
KanaevaEkaterina <kanaeva.katerina6@gmail.com>
2016-11-30 17:44:47 +0100

    add some changes, just not to merge

--- Exercise2/harris.m
+++ Exercise2/harris.m
@@ -6,9 +6,9 @@ R = response(I, sigma_d, sigma_i, alpha);
 
 R = matrix_rescale(R,-1000,1000);
 %find regional max
-%V = imregionalmax(R) .* R;
+V = imregionalmax(R) .* R;
 %filter
-V = (R>threshold).*R;
+V = (V>threshold).*V;
 %get coordinates for all filtered corners
 [r,c] = find(V);
 
