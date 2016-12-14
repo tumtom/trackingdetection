75caa37a81a4adc78cae5e966c6e3538d5e7d532
tom <thomas.rehner@tum.de>
2016-12-14 20:47:56 +0100

    Exercise 6 Task 1+2

---
+++ Exercise6/exer.m
@@ -0,0 +1,23 @@
+I_0 = single(rgb2gray(imread('img_sequence/0000.png')));
+
+A = [472.3 0.64 329.0; 0 471.0 268.3; 0 0 1];
+
+run('/home/tom/ownCloud/Skripte/Master/Semester3_WS1617/TrackingDetection/Excercises/Exercise3/vlfeat/toolbox/vl_setup.m')
+
+[fs, ds] = vl_sift(I_0);
+points = [];
+points(1,:) = round(fs(1,:));
+points(2,:) = round(fs(2,:));
+obj_points=[];
+d_obj_points=[];
+for i=1:size(points,2)
+    if points(1,i) > 95 && points(1,i) < 557 && points(2,i) > 65 && points(2,i) < 390
+        obj_points=[obj_points,points(:,i)];
+        d_obj_points=[d_obj_points,ds(:,i)];
+    end
+end
+
+obj_points_hom = ones(3, size(obj_points,2));
+obj_points_hom(1:2, :) = obj_points;
+
+M = A^(-1)*obj_points_hom;
---
+++ Exercise6/exer2.m
@@ -0,0 +1,17 @@
+%use results of exer.m
+run('exer.m');
+
+nrImages = 44;
+
+%loop trough all images
+for i=1:nrImages
+    I_i_raw = imread(strcat('img_sequence/',num2str(i,'%04d'),'.png'));
+    I_i = single(rgb2gray(I_i_raw));
+    [fs, ds] = vl_sift(I_i);
+    [matches, scores] = vl_ubcmatch(d_obj_points, ds);
+    tform = estimateGeometricTransform(obj_points(:,matches(1,:))',fs(1:2,matches(2,:))','projective','Confidence',97);
+    outputView = imref2d(size(I_0));
+    Ir = imwarp(I_i_raw, tform, 'OutputView', outputView);
+    %save it to a folder called results (should exist!)
+    imwrite(Ir,strcat('results/restored_',num2str(i,'%04d'),'.png'));
+end
